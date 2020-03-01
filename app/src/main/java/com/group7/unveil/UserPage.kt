e700798f9d320a36c7197786c0b16a2a877abd42
nataliachmurak <natalia.chmurak@gmail.com>
2020-03-01 22:51:16 +0100

    I connected user page with settings - User page is in settings now as it was easier than the other way around. Added some buttons functionalities

--- app/src/main/java/com/group7/unveil/UserPage.kt
+++ app/src/main/java/com/group7/unveil/UserPage.kt
@@ -10,10 +10,18 @@ import android.hardware.SensorManager
 import android.os.Bundle
 import android.widget.ImageView
 import android.provider.ContactsContract
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.SeekBar
 import com.group7.unveil.data.StepData
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SwitchCompat
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.ui.AppBarConfiguration
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.material.navigation.NavigationView
 import com.group7.unveil.stepCounter.StepDetector
 import com.group7.unveil.stepCounter.StepListener
 import kotlinx.android.synthetic.main.activity_main_page.*
@@ -27,9 +35,12 @@ class UserPage : AppCompatActivity(), SensorEventListener, StepListener {
     lateinit var sensorManager: SensorManager
     lateinit var sensor: Sensor
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_page)
+
+
         //setSupportActionBar(toolbar)
 
 //        fab.setOnClickListener { view ->
